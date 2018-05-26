3951879b6bb86cb09ff80a9446bf3c07e2330a57
Unknown <p15148@students.cs.unipi.gr>
2018-05-26 19:02:10 +0300

    adds 6.17

---
+++ ex6.17/ex6.16-coder-modified.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import numpy as np
+
+Q_PARAMETER = 10
+IMAGE_PATH = 'frames/'
+DEST_PATH = 'compressed_frames/'
+IMAGE_NAME = 'frame_1'
+IMAGE_TYPE = '.bmp'
+
+img = np.array(Image.open(IMAGE_PATH + IMAGE_NAME + IMAGE_TYPE).convert('L'))
+img[0, 0] = int(round(img[0, 0] / Q_PARAMETER))
+start_flag = True
+run = img[0, 0]
+length = 1
+output = str(img.shape[0]) + 'x' + str(img.shape[1]) + '|' + str(Q_PARAMETER) + '|'
+for i in range(img.shape[0]):
+    for j in range(img.shape[1]):
+        if start_flag:
+            start_flag = False
+            continue
+
+        img[i, j] = int(round(img[i, j] / Q_PARAMETER))
+        if img[i, j] != run:
+            if length != 1:
+                output += str(run) + ',' + str(length) + ';'
+                run = img[i, j]
+                length = 1
+            else:
+                output += str(run) + ';'
+                run = img[i, j]
+        else:
+            length += 1
+if length != 1:
+    output += str(run) + ',' + str(length) + ';'
+else:
+    output += str(run) + ';'
+file = open(DEST_PATH + 'compressed_' + IMAGE_NAME, 'w')
+file.write(output)
+file.close()
