79c009fb8bb09a6d9951cf6395a0716e8a0eec3d
Unknown <p15148@students.cs.unipi.gr>
2018-05-27 14:56:21 +0300

    6.16 coder modified for 6.17

--- ex6.17/ex6.16-coder-modified.py
+++ ex6.17/ex6.16-coder-modified.py
@@ -4,36 +4,38 @@ import numpy as np
 Q_PARAMETER = 10
 IMAGE_PATH = 'frames/'
 DEST_PATH = 'compressed_frames/'
-IMAGE_NAME = 'frame_1'
+IMAGE_NAME = 'frame_'
 IMAGE_TYPE = '.bmp'
+N_FRAMES = 60
 
-img = np.array(Image.open(IMAGE_PATH + IMAGE_NAME + IMAGE_TYPE).convert('L'))
-img[0, 0] = int(round(img[0, 0] / Q_PARAMETER))
-start_flag = True
-run = img[0, 0]
-length = 1
-output = str(img.shape[0]) + 'x' + str(img.shape[1]) + '|' + str(Q_PARAMETER) + '|'
-for i in range(img.shape[0]):
-    for j in range(img.shape[1]):
-        if start_flag:
-            start_flag = False
-            continue
+for frame in range(1, N_FRAMES + 1):
+    img = np.array(Image.open(IMAGE_PATH + IMAGE_NAME + str(frame) + IMAGE_TYPE).convert('L'))
+    img[0, 0] = int(round(img[0, 0] / Q_PARAMETER))
+    start_flag = True
+    run = img[0, 0]
+    length = 1
+    output = str(img.shape[0]) + 'x' + str(img.shape[1]) + '|' + str(Q_PARAMETER) + '|'
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if start_flag:
+                start_flag = False
+                continue
 
-        img[i, j] = int(round(img[i, j] / Q_PARAMETER))
-        if img[i, j] != run:
-            if length != 1:
-                output += str(run) + ',' + str(length) + ';'
-                run = img[i, j]
-                length = 1
+            img[i, j] = int(round(img[i, j] / Q_PARAMETER))
+            if img[i, j] != run:
+                if length != 1:
+                    output += str(run) + ',' + str(length) + ';'
+                    run = img[i, j]
+                    length = 1
+                else:
+                    output += str(run) + ';'
+                    run = img[i, j]
             else:
-                output += str(run) + ';'
-                run = img[i, j]
-        else:
-            length += 1
-if length != 1:
-    output += str(run) + ',' + str(length) + ';'
-else:
-    output += str(run) + ';'
-file = open(DEST_PATH + 'compressed_' + IMAGE_NAME, 'w')
-file.write(output)
-file.close()
+                length += 1
+    if length != 1:
+        output += str(run) + ',' + str(length) + ';'
+    else:
+        output += str(run) + ';'
+    file = open(DEST_PATH + 'compressed_' + IMAGE_NAME + str(frame), 'w')
+    file.write(output)
+    file.close()
